d160d9672b0cde73c2cc6ec15b1d90abf8756f93
Youssefsaad73 <73830904+Youssefsaad73@users.noreply.github.com>
2023-03-24 01:36:16 +0200

    done

--- event_brite_app/lib/main.dart
+++ event_brite_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'screens/splash_screen/splash_screen.dart';
+import '../screens/splash_screen/splash_screen.dart';
 
 
 void main() {
--- event_brite_app/lib/reusable_widgets/profile_custom_container.dart
+++ event_brite_app/lib/reusable_widgets/profile_custom_container.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../screens/settings_screen/settings_screen.dart';
+import '../../screens/settings_screen/settings_screen.dart';
 
 class ProfileSettingsContainer extends StatelessWidget {
   const ProfileSettingsContainer(
